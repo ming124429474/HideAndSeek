401bde25c402cb95ab3bc77e73186b6bcd5309ab
ming <124429474@qq.com>
2019-10-20 12:03:34 +0800

    匹配机制

---
+++ App/Lib/Redis.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 2019/10/20
+ * Time: 11:02
+ */
+
+namespace App\Lib;
+
+
+class Redis
+{
+
+}
---
+++ App/Manager/Logic.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 2019/10/20
+ * Time: 10:54
+ */
+
+namespace App\Manager;
+
+
+class Logic
+{
+
+}
